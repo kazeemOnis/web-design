df3bfb472c30c0da314637cf9e41bceccc11a0d4
Kazeem Onisarotu <konisarotu@yahoo.com>
2017-09-25 02:32:05 +0100

    the grunt and gulp added

---
+++ Bootstrap4/conFusion/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#mycarousel").carousel({interval:2000});
+    $("#carousel-button").click(function(){
+      if($("#carousel-button").children("span").hasClass("fa-pause")){
+        $("#mycarousel").carousel('pause');
+        $("#carousel-button").children("span").removeClass("fa-pause");
+        $("#carousel-button").children("span").addClass("fa-play");
+      }
+      else if($('#carousel-button').children("span").hasClass("fa-play")){
+        $("#mycarousel").carousel('cycle');
+        $("#carousel-button").children("span").removeClass("fa-play");
+        $("#carousel-button").children("span").addClass("fa-pause");
+      }
+
+   });
+  $('#reserveBtn').click(function(){
+    $('#reserveModal').modal();
+  });
+  $('#loginBtn').click(function(){
+    $('#loginModal').modal();
+  });       
+});
